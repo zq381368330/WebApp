158d73a51bbac9ecebb7d1d7ab822af28a29217d
zq381368330 <381368330@qq.com>
2016-12-14 18:31:05 +0800

    first commit

---
+++ js/H5ComponentBar.js
@@ -0,0 +1,33 @@
+/*柱图组件对象*/
+var H5ComponentBar=function( name,cfg){
+ var component=new H5ComponentBase(name,cfg);
+
+$.each(cfg.data,function(idx,item){
+    
+
+var line=$('<div class="line">');
+var name=$('<div class="name">');
+var rate=$('<div class="rate">');
+var per=$('<div class="per">');
+var width=item[1]*100+'%';
+var bgStyle='';
+
+if( item[2]){
+ bgStyle='style="background-color:'+item[2]+'"';
+
+
+}
+//bg元素操作动画，从百分之0到百分之100
+rate.html( '<div class="bg" '+bgStyle+'></div>' );
+rate.css('width',width);
+name.text(  item[0]);
+per.text(width);
+
+line.append( name ).append( rate ).append( per );
+component.append(line);
+
+
+});
+
+  return component;
+}
